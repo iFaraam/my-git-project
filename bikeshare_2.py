0298e2656bb4f2b022da1e91243d99a547ce92ec
Faisal Alshammari <faisalfha@outlook.com>
2023-06-17 18:03:12 +0300

    docs: Enhance function documentation

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -8,12 +8,12 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Prompts user for input and returns selected city, month, and day for data analysis.
 
     Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) city - User-selected city to analyze. Must be either 'chicago', 'new york city', or 'washington'.
+        (str) month - User-selected month to filter data by, or "all" to apply no month filter.
+        (str) day - User-selected day of the week to filter data by, or "all" to apply no day filter.
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
@@ -51,15 +51,16 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city, filtering by month and day if applicable.
+
     Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) city - User-selected city to analyze.
+        (str) month - User-selected month to filter data by, or "all" to apply no month filter.
+        (str) day - User-selected day of the week to filter data by, or "all" to apply no day filter.
+
     Returns:
-        df - pandas DataFrame containing city data filtered by month and day
+        df - pandas DataFrame containing city data filtered by month and day.
     """
-    
     # load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
     # convert the Start Time column to datetime
@@ -85,10 +86,10 @@ def load_data(city, month, day):
 
 def display_data(df):
     """
-    Displays rows of data upon user request.
+    Displays rows of data in response to user's request.
 
     Args:
-        data_frame (pandas.DataFrame): The DataFrame containing the data.
+        df (pandas.DataFrame): The DataFrame containing the data to display.
 
     Returns:
         None
@@ -102,8 +103,15 @@ def display_data(df):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """
+    Calculates and displays statistics on the most frequent times of travel.
 
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the data.
+
+    Returns:
+        None
+    """
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
@@ -124,8 +132,15 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """
+    Calculates and displays statistics on the most popular stations and trip.
 
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the data.
+
+    Returns:
+        None
+    """
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
@@ -149,84 +164,87 @@ def station_stats(df):
     end_station = common_comb_index[1]
     count = stations_combined[common_comb_index]
 
-    print("Most common start station and end station combination: ", start_station, "-", end_station, "Count: ", count)
+    print("Most frequent trip from {} to {}, was made {} times".format(start_station, end_station, count))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """
+    Calculates and displays statistics on the total and average trip duration.
 
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the data.
+
+    Returns:
+        None
+    """
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
     # display total travel time
-    total_trip = df['Trip Duration'].sum()
-    print("Total travel time in Hours: ", total_trip / 3600, "Hours")
-
+    total_travel = df['Trip Duration'].sum()
+    print("Total Travel Time: ", total_travel)
 
     # display mean travel time
-    mean_travel_time = df['Trip Duration'].mean()
-    print("Mean travel time in Minutes: ", mean_travel_time / 60, "Minutes")
-
+    mean_travel = df['Trip Duration'].mean()
+    print("Mean Travel Time: ", mean_travel)
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
-def user_stats(df):
-    """Displays statistics on bikeshare users."""
+def user_stats(df, city):
+    """
+    Calculates and displays statistics on bikeshare users.
 
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the data.
+        city (str): The name of the city.
+
+    Returns:
+        None
+    """
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
     # Display counts of user types
-    user_types_count = df['User Type'].value_counts()
-    print("User types:\n", user_types_count)
+    user_type = df['User Type'].value_counts()
+    print("Displaying counts of user types:\n", user_type)
 
-    try:
+    if city != 'washington': # The washington file does not have gender and birth year column
         # Display counts of gender
-        gender_counts = df['Gender'].value_counts()
-        print('Gender counts:\n', gender_counts)
-    except KeyError:
-        # Handle the case when 'Gender' column is not present
-        print('Gender information not available.')
+        gender_count = df['Gender'].value_counts()
+        print("Displaying counts of gender:\n", gender_count)
 
-    try:
         # Display earliest, most recent, and most common year of birth
-        earliest_year = df['Birth Year'].min()
-        print("The earliest birth year:\n", earliest_year)
+        earliest_birthyear = df['Birth Year'].min()
+        print("Displaying the earliest year of birth: ", earliest_birthyear)
 
-        recent_year = df['Birth Year'].max()
-        print("The most recent birth year:\n", recent_year)
+        most_recent_birthyear = df['Birth Year'].max()
+        print("Displaying the most recent year of birth: ", most_recent_birthyear)
 
-        common_year = df['Birth Year'].mode()[0]
-        print("The most common birth year:\n", common_year)
-    except KeyError:
-        # Handle the case when 'Birth Year' column is not present
-        print('Birth year information not available.')
+        most_common_birthyear = df['Birth Year'].mode()[0]
+        print("Displaying the most common year of birth: ", most_common_birthyear)
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-' * 40)
-
+    print('-'*40)
 
 
 def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
+        display_data(df)
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
-        user_stats(df)
-        display_data(df)
+        user_stats(df, city)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
-
 if __name__ == "__main__":
-	main()
+    main()
